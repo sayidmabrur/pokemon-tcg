29151630f48941bec6ea6ce7032bd39df47e936a
sayid <sayid.10121012@mahasiswa.unikom.ac.id>
2026-07-29 00:13:12 +0700

    added policy

--- archetypes/alakazam/policy_network/policy.py
+++ archetypes/alakazam/policy_network/policy.py
@@ -1,11 +1,34 @@
 
+from pprint import pprint
+from dataclasses import dataclass
+from typing import List
+
 import torch
 import torch.nn as nn
 
 from torch.utils.data import Dataset, DataLoader
 from dataset import PolicyFeatureDataset
 
-from dataset import PolicyFeatureDataset
+@dataclass
+class OptionsVocab:
+    area: int
+    attackId: int
+    count: float
+    energyIndex: int
+
+
+    pass
+
+@dataclass
+class SelectionVocab:
+    pass
+
+@dataclass
+class DecisionContextVocab:
+    options: List[OptionsVocab]
+    Selection: List[SelectionVocab]
+
+    pass
 
 class PolicyNetwork(nn.Module):
 
@@ -18,11 +41,11 @@ class PolicyNetwork(nn.Module):
 
 policy = PolicyNetwork()
 
-from pprint import pprint
 
-dataset = PolicyFeatureDataset("data/policy_decisions.parquet", player_name="Yushin Ito")
+dataset = PolicyFeatureDataset(
+    "data/policy_decisions.parquet", player_name="Yushin Ito")
 print("dataset sample:")
 print("==="*50)
-pprint( dataset[0])
+pprint(dataset[0])
 print("==="*50)
-print(f"decision length: {len(dataset[0][0]['features']['decision_chain'])}")
+print(f"decision length: {len(dataset[9][0]['features']['decision_chain'])}")
